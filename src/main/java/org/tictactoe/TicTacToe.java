90ce5ca53b1d7eede929c3487c4fe94613685142
vitaFlash <vitaliaflash@gmail.com>
2025-05-15 00:43:25 +0200

    feat:nuevos metodos finished

--- src/main/java/org/tictactoe/TicTacToe.java
+++ src/main/java/org/tictactoe/TicTacToe.java
@@ -58,4 +58,38 @@ public class TicTacToe {
         return newPositions;
     }
 
+    static boolean checkWin(char currentPlayer) {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+        }
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+            return true;
+        }
+        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean checkDraw() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '_') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
