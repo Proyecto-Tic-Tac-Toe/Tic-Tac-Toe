0c1cd3bcaba719759a0ac372e00fe983b9bc8766
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-05-15 15:21:57 +0200

    Revert "feat:nuevos metodos finished"
    This reverts commit 90ce5ca53b1d7eede929c3487c4fe94613685142.

--- src/main/java/org/tictactoe/Main.java
+++ src/main/java/org/tictactoe/Main.java
@@ -6,40 +6,11 @@ import static org.tictactoe.TicTacToe.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        printBoard();
+        placeNewPosition(checkCorrectPosition(captureNewPosition()), 'x');
+        printBoard();
+        placeNewPosition(checkCorrectPosition(captureNewPosition()), 'x');
+        printBoard();
 
-        System.out.println("███╗   ██╗███████╗██╗    ██╗     ██████╗  █████╗ ███╗   ███╗███████╗");
-        System.out.println("████╗  ██║██╔════╝██║    ██║    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝");
-        System.out.println("██╔██╗ ██║█████╗  ██║ █╗ ██║    ██║  ███╗███████║██╔████╔██║█████╗  ");
-        System.out.println("██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  ");
-        System.out.println("██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗");
-        System.out.println("╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝");
-
-        char currentPlayer = 'x';
-        boolean gameOver = false;
-
-        while (!gameOver) {
-            printBoard();
-            placeNewPosition(checkCorrectPosition(captureNewPosition()), currentPlayer);
-            printBoard();
-            if(checkWin(currentPlayer)){
-                System.out.println("Player " + currentPlayer + " wins!");
-                gameOver = true;
-            } else if (checkDraw()) {
-                ;
-                System.out.println("It's a draw!");
-                gameOver = true;
-            }
-            currentPlayer = (currentPlayer == 'x') ? 'o' : 'x';
-        }
-
-        System.out.println("  ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ ");
-        System.out.println(" ██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗");
-        System.out.println(" ██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝");
-        System.out.println(" ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║██║   ██║██╔══╝  ██╔══██╗");
-        System.out.println(" ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝╚██████╔╝███████╗██║  ██║");
-        System.out.println("  ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝");
-
-            }
-        }
-
-
+    }
+}
